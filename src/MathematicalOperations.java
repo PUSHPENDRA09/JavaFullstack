e166280ff1fb135f3f8deceed1f336ceb2274c8a
PCC <patilpushpendra01@gmail.com>
2022-06-08 16:09:15 +0530

    initial commit

---
+++ src/MathematicalOperations.java
@@ -0,0 +1,24 @@
+public class MathematicalOperations {
+    public static void main(String[] args) {
+        int x = 52;
+        int y = 55;
+        int result;
+
+        //addition
+        result = x + y;
+        System.out.println("The addition is" + result);
+
+        //substraction
+
+        result = x - y;
+        System.out.println("The result is" + result);
+
+        //multiplication
+        result = x * y;
+        System.out.println("The result is" + result);
+
+        //division
+        result = x / y;
+        System.out.println("The result is" + result);
+    }
+}
