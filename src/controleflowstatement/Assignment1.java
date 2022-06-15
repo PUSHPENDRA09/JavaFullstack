7f17ff069c54ae8a406d9b921b5f2cdca9667805
PCC <patilpushpendra01@gmail.com>
2022-06-15 15:48:50 +0530

    VowelsEx

--- src/controleflowstatement/Assignment1.java
+++ src/controleflowstatement/Assignment1.java
@@ -3,11 +3,27 @@ package controleflowstatement;
 public class Assignment1 {
     public static void main(String args[]) {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println("");
+//        }
+//
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" * ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
-    }
+
+
+//        for (int i = 1; i < 6; i++) {
+//            for (int j = 6; j > i; j--) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println(" ");
+//
+//        }
+  }
 }
---
+++ src/controleflowstatement/SwitchStatement.java
@@ -0,0 +1,26 @@
+package controleflowstatement;
+
+public class SwitchStatement {
+    public static void main(String args[]) {
+
+        int ch = 3;
+        switch (ch) {
+
+            case 1:
+                System.out.println("one");
+                break;
+
+            case 2:
+                System.out.println("two");
+                break;
+
+            case 3:
+                System.out.println("three");
+                break;
+
+            default:
+                System.out.println("no match found");
+                break;
+        }
+    }
+}
