ef971d2096f8e1e42d3f61e606fe0beb3e50ce79
PCC <patilpushpendra01@gmail.com>
2022-08-04 17:57:01 +0530

    Electricity Bill

--- src/exceptionHandeling/ElectricityBill.java
+++ src/exceptionHandeling/ElectricityBill.java
@@ -12,12 +12,17 @@ public class ElectricityBill {
         double bill = 0;
         if (units <= 50) {
             bill = units * 0.5;
-        } else if (units <= 150) {
-            bill = 50 * 0.5 + 100 * 0.75 + (units - 150) * 1.20;
-        } else {
+        }
+        else if (units <= 150) {
+            bill = 50 * 0.5  + (units - 50) * 0.75;
+        }
+        else if (units <=250) {
+            bill=50*0.5+100*0.75+(units-150)*1.20;
+        }
+        else {
             bill = 50 * 0.5 + 100 * 0.75 + 100 * 1.20 + (units - 250) * 1.50;
         }
-        double total = bill + (20 / 100 * bill);
+        double total = bill + ((20 * bill) / 100);
         System.out.println("The total bill is " + total);
     }
 }
