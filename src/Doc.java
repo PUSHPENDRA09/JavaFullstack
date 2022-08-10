9825c9a5ea4c1caf165fe517956bd276f31b9842
PCC <patilpushpendra01@gmail.com>
2022-08-10 18:26:52 +0530

    MysqlSelectionDemo

---
+++ src/Doc.java
@@ -0,0 +1,23 @@
+
+
+//foregin key
+//1.unique
+//2.not null
+
+//unique key
+//1.can be  null
+
+//types of join
+//
+//1.inner joint
+//2.left joint
+//3.right join
+//4.self join
+
+
+///commands for sql
+
+//1.show databases
+//2.use database(name of database)
+//3. show tables
+//4. select * from
